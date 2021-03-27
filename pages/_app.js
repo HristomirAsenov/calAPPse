85570517c0bf4e86c75118d5dd0a47dc9f49084a
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 16:40:26 +0200

    fix relative paths

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import Layout from '../components/layout';
+import Layout from 'components/layout';
 
-import '../styles/globals.scss';
+import 'styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
     return <Layout>
