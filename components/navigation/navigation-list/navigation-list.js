26331e9bb45e92b76b916197ab2a6c77adae37f6
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 14:22:54 +0200

    create navigation-list component

---
+++ components/navigation/navigation-list/navigation-list.js
@@ -0,0 +1,21 @@
+import NavigationListItem from '../navigation-list-item';
+
+import classes from './navigation-list.module.scss';
+
+const NavigationList = ({
+    links,
+}) => (
+    <ul className={classes['navigation-list']}>
+        {
+            links.map(({ name, href }) => (
+                <NavigationListItem
+                    name={name}
+                    href={href}
+                    key={ href }
+                />)
+            )
+        }
+    </ul>
+);
+
+export default NavigationList;
