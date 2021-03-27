ee455aeee34c3b384c5186dce4aefee887f15353
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 14:22:34 +0200

    create navigation component

---
+++ components/navigation/navigation-constants.js
@@ -0,0 +1,14 @@
+export const MAIN_LINKS = [
+    {
+        name: 'Home',
+        href: '/',
+    },
+    {
+        name: 'SignIn',
+        href: '/sign-in',
+    },
+    {
+        name: 'SignUp',
+        href: '/sign-up',
+    },
+];
