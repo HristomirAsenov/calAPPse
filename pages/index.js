463c9c1678e1303ff6c859a9beba036410e1f755
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 17:51:31 +0200

    beutify homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
+import Link from 'next/link';
+
 import classes from './index.module.scss';
+
+import { MONTHS } from 'globalConstants'
+import { capitelizeString } from 'utils';
 
 function HomePage() {
     return (
         <div className={ `${classes['home-container']} container` }>
-            <img
-                className={ classes['home-hero-banner'] }
-                src={ '/images/home-hero-banner.jpg' }
-                alt='calAPPse'
-            />
+
+            <div className={ classes.months }>
+                {
+                    MONTHS.map(({ name, order }) => <div className={ classes.month } key={ name }>
+                        <Link
+                            href={ {
+                                pathname: '/months/[month]',
+                                query: {
+                                    month: name,
+                                },
+                            } }
+                        >
+                            <a className={ classes['month-link'] }>
+                                { capitelizeString(name) }
+                            </a>
+                        </Link>
+                    </div>)
+                }
+            </div>
         </div>
     );
-}
+};
 
 export default HomePage;
