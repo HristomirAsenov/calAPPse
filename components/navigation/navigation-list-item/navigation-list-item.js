1e9e8fd0e369d32c05bcf247503a9bca71bb36d9
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 14:54:39 +0200

    update navigation-list-item

--- components/navigation/navigation-list-item/navigation-list-item.js
+++ components/navigation/navigation-list-item/navigation-list-item.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 
+import classes from './navigation-list-item.module.scss';
+
 const NavigationListItem = ({
     name,
     href,
 }) => (
-    <li>
-        <Link href={ href } >
+    <li className={ classes['navigation-list-item'] }>
+        <Link href={ href }>
             { name }
         </Link>
     </li>
