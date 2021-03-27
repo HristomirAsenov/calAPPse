e6bdb50d537e5f7946e261a490437a400c29dd89
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 18:43:58 +0200

    create dummy page for save record

--- components/layout/main-header/navigation/navigation-constants.js
+++ components/layout/main-header/navigation/navigation-constants.js
@@ -4,7 +4,7 @@ export const MAIN_LINKS = [
         href: '/',
     },
     {
-        name: 'Save New Record',
+        name: 'Save Record',
         href: '/save-record',
         prefix: () => (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
---
+++ pages/save-record/index.js
@@ -0,0 +1,10 @@
+const SaveNewRecord = () => {
+
+    return (
+        <div className='container'>
+            <h1>Save new record</h1>
+        </div>
+    );
+};
+
+export default SaveNewRecord;
