67d598d53150c4ca77ef6dddf26e73f709a17fff
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 18:07:10 +0200

    create getDaysInMonth util

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,18 @@
+import { MONTHS } from 'globalConstants';
+
 export const capitelizeString = (string) => {
     return string[0].toUpperCase().concat(string.slice(1).toLowerCase());
+};
+
+/**
+ * 
+ * @param {Number} monthOrder 
+ * @returns
+ */
+export const getDaysInMonth = (month) => {
+    const selectedMonth = MONTHS.find(m => m.name === month);
+
+    if (selectedMonth) {
+        return new Date(2021, selectedMonth.order, 0).getDate();
+    }
 };
