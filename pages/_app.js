b4ce587eeda657c885640a7cbf7fd502b404d23c
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 14:23:47 +0200

    adjust _app.js file after navigation update

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,11 @@
-import Link from 'next/link';
-import '../styles/globals.css';
+import Navigation from '../components/navigation';
+
+import '../styles/globals.scss';
 
 function MyApp( { Component, pageProps } ) {
     return (
-        <section>
-            <ul>
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/months">Months</Link>
-                </li>
-                <li>
-                    <Link href={ {
-                        pathname: '/months/[month]',
-                        query: { month: 'june' }
-                    } }
-                    >
-                        June
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/months/june/days/20"> 20 June</Link>
-                </li>
-                <li>
-                    <Link
-                        href="/nested/example/so/fking/nested/query/im/lost"
-                    >
-                        Nested Example (you may try it with different query)
-                    </Link>
-                </li>
-            </ul>
+        <section className='site-wrapper'>
+            <Navigation />
             <Component { ...pageProps } />
         </section>
     );
