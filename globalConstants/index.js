b44ded80027c571777339a275efa18666ac21aa3
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-27 17:51:02 +0200

    create constants and utils folders

---
+++ globalConstants/index.js
@@ -0,0 +1,50 @@
+export const MONTHS = [
+    {
+        name: 'january',
+        order: 1,
+    },
+    {
+        name: 'february',
+        order: 2,
+    },
+    {
+        name: 'march',
+        order: 3,
+    },
+    {
+        name: 'april',
+        order: 4,
+    },
+    {
+        name: 'may',
+        order: 5,
+    },
+    {
+        name: 'june',
+        order: 6,
+    },
+    {
+        name: 'july',
+        order: 7,
+    },
+    {
+        name: 'august',
+        order: 8,
+    },
+    {
+        name: 'september',
+        order: 9,
+    },
+    {
+        name: 'october',
+        order: 10,
+    },
+    {
+        name: 'november',
+        order: 11,
+    },
+    {
+        name: 'december',
+        order: 12,
+    },
+];
---
+++ utils/index.js
@@ -0,0 +1,3 @@
+export const capitelizeString = (string) => {
+    return string[0].toUpperCase().concat(string.slice(1).toLowerCase());
+};
