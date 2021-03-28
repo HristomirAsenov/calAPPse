ba5a2fffe83d2aa59066b183fcf617cd74c6be0e
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-29 00:04:43 +0300

    housekeeping

--- pages/months/[month]/[day]/index.js
+++ pages/months/[month]/[day]/index.js
@@ -1,14 +1,15 @@
 import { useRouter } from 'next/router';
 
-function SelectedMonthDayPage() {
+const SelectedMonthDayPage = () => {
     const router = useRouter();
     const { month, day } = router.query;
+    
     return (
         <div className='container'>
             <h1>Selected Month ({ month })</h1>
             <h1>Selected Day  ({ day })</h1>
         </div>
-    )
-}
+    );
+};
 
 export default SelectedMonthDayPage;
--- pages/months/[month]/index.js
+++ pages/months/[month]/index.js
@@ -5,20 +5,19 @@ import MonthDayList from 'components/month-day-list';
 
 import classes from './index.module.scss';
 
-function SelectedMonthPage() {
+const SelectedMonthPage = () => {
     const router = useRouter();
     const { month } = router.query;
 
     const daysInSelectedMonth = getDaysInMonth(month);
     const days = Array.from([...Array(daysInSelectedMonth)].keys());
 
-    console.log(month);
     return (
         <div className={ `${classes['month-container']} container` }>
             <h1>{ month && capitelizeString(month) }</h1>
             <MonthDayList days={ days } />
         </div>
-    )
-}
+    );
+};
 
 export default SelectedMonthPage;
