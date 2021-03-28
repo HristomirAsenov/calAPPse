e1a96b9d938b6a47e919284a73249c5997e6344f
Hristomir Asenov <37121246+HristomirAsenov@users.noreply.github.com>
2021-03-28 17:01:36 +0300

    show each month's season icon

--- components/month-list/month-list-item/month-list-item.js
+++ components/month-list/month-list-item/month-list-item.js
@@ -2,29 +2,45 @@ import Link from 'next/link';
 
 import { capitelizeString } from 'utils';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faLeaf, faCloudSunRain, faSnowflake } from '@fortawesome/free-solid-svg-icons'
+
 import classes from './month-list-item.module.scss';
 
 const MonthListItem = ({
     name,
-}) => (
-    <div className={ classes.month }>
-        <Link
-            href={ {
-                pathname: '/months/[month]',
-                query: {
-                    month: name,
-                },
-            } }
-        >
-            <a
-                title={ `Check ${capitelizeString(name)} records` }
-                className={ classes['month-link'] }
-            >
-                { capitelizeString(name) }
-            </a>
-        </Link>
+    season,
+}) => {
 
-    </div>
-);
+    const SeasonIcon = () => {
+        switch (season) {
+            case 'spring': return <FontAwesomeIcon icon={ faLeaf } />
+            case 'summer': return <FontAwesomeIcon icon={ faSun } />
+            case 'autumn': return <FontAwesomeIcon icon={ faCloudSunRain } />
+            case 'winter': return <FontAwesomeIcon icon={ faSnowflake } />
+        }
+    };
+
+    return (
+        <div className={ classes.month }>
+            <Link
+                href={ {
+                    pathname: '/months/[month]',
+                    query: {
+                        month: name,
+                    },
+                } }
+            >
+                <a
+                    title={ `Check ${capitelizeString(name)} records` }
+                    className={ classes['month-link'] }
+                >
+                    { capitelizeString(name) }
+                </a>
+            </Link>
+            <SeasonIcon />
+        </div>
+    );
+}
 
 export default MonthListItem;
